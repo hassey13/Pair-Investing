d5eb0c3e32686d0b645447719d847b41b4b8ed77
hassey13 <ehasselbring@gmail.com>
2017-03-29 12:28:08 -0400

    route change for adapters

--- src/adapters/stockAdapter.js
+++ src/adapters/stockAdapter.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000/api/v1/'
+axios.defaults.baseURL = 'https://connected-investing-api.herokuapp.com/api/v1/'
 axios.defaults.headers.common['AUTHORIZATION'] = sessionStorage.getItem('jwt')
 
 export const stockAdapter = {
